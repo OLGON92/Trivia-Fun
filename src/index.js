37da7c6e0a6926477e326db10be3e7cb66ec1e54
OLGON92 <ogon318@gmail.com>
2022-10-23 16:28:13 -0700

    add UI logic

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Host from './js/trivia.js'
 
+window.addEventListener("load", start);
 
 async function apiCall(){
-  let response = await fetch('https://opentdb.com/api.php?amount=10');
+  let response = await fetch('https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple');
   console.log(response)
   let data = await response.json()
-  console.log(data)
-  triviaData(data)
+  return data;
 }
 
-function triviaData(data){}
+function start()
+{
+let One = new Host(apiCall());
+console.log(One)
+console.log(One.func);
+}
+
+
