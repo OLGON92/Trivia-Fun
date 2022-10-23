b7901d8402f6a533ab00a9fe44b17290081fcf92
OLGON92 <ogon318@gmail.com>
2022-10-23 16:27:25 -0700

    add business logic

---
+++ src/js/trivia.js
@@ -0,0 +1,31 @@
+export default class Host {
+    constructor(data) { 
+        this.data = data;
+    }
+
+func () 
+{
+	let returnarray = [0, 1, 2, 3, 4, 5];
+  
+  returnarray[0] = this.data.results[0].question;
+  let arraynums = [1, 2, 3, 4]
+  
+    let i = 4;
+    let j = 0;
+    let temp;
+
+    while (i--) {
+        j = Math.floor(Math.random() * (i+1));
+        temp = arraynums[i];
+        arraynums[i] = arraynums[j];
+        arraynums[j] = temp;
+    }
+   returnarray[arraynums[0]] = this.data.results[0].correct_answer;
+   returnarray[arraynums[1]] = this.data.results[0].incorrect_answers[0];
+   returnarray[arraynums[2]] = this.data.results[0].incorrect_answers[1];
+   returnarray[arraynums[3]] = this.data.results[0].incorrect_answers[2];
+   returnarray[5] = arraynums[0];
+   
+return returnarray;
+}
+}
