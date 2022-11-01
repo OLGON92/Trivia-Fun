988ed2b535e618a01f050764cab7efa712259adb
RemyXVX <remyfranciscoflores@gmail.com>
2022-10-31 21:16:38 -0700

    cleaned up the code, our async/await call works perfectly

--- src/js/call.js
+++ src/js/call.js
@@ -1,40 +1,15 @@
 export default class Call {
-  static async callStuff(category, difficulty, type) {
+  static async triviaStuff(category, level, type) {
     try {
       const response = await fetch(`https://opentdb.com/api.php?amount=10&category=31&difficulty=medium&type=multiple`);
       const jsonResponse = await response.json();
       if(!response.ok) {
         const errorMessage = `${response.status} ${response.statusText} ${jsonResponse.message}`;
-        console.log(errorMessage);
         throw new Error (errorMessage);
       }
-      console.log(jsonResponse);
       return jsonResponse;
     } catch(error) {
       return error;
     }
   }
 }
-
-  /*let response = await fetch('https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple');
-  console.log(response)
-  //let data = await response.json()
-  let request = new XMLHttpRequest();
-
-  const url = `https://opentdb.com/api.php?amount=10&category=15&difficulty=medium&type=multiple`;
-  request.addEventListener("loadend", function() {
-    const response = JSON.parse(this.responseText);
-    if (this.status === 200) {
-      console.log(response);
-    }
-    else 
-    {
-      console.log(`Error is ${this.status}`)
-    }
-  });
-
-  request.open("GET", url, true);
-  request.send();
-  console.log(response);*/
-
-  //return response;
